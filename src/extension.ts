3e0c495d8a7524f300a0ef16145e4d638f4bd9a7
shota.miyahira <shota.miyahira@stafes.com>
2022-12-14 15:16:19 +0900

    使えるようにした

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,75 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { writeFileSync } from "fs";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "create-react-component-files" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "create-component-files" is now active!');
+  let disposable = vscode.commands.registerCommand(
+    "create-react-component-files.createFile",
+    async (sender) => {
+      try {
+        console.log(1, sender);
+        if (!sender.path) return;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('create-component-files.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from create_component_files!');
-	});
+        console.log(2);
 
-	context.subscriptions.push(disposable);
+        console.log(3);
+        // コンポーネント名を入力するダイアログを表示
+        const componentName = await vscode.window.showInputBox({
+          title: "コンポーネント名を入力してください",
+        });
+
+        // 入力がなければ処理を終了
+        if (componentName === undefined) {
+          return;
+        }
+        console.log(4);
+
+        // vscode.;
+        const newDir = vscode.Uri.joinPath(sender, componentName);
+        console.log(5, newDir.path);
+
+        await vscode.workspace.fs.createDirectory(newDir);
+
+        const indexFile = vscode.Uri.joinPath(newDir, "index.ts");
+        const indexFileBlob: Uint8Array = Buffer.from(
+          `export * from "./${componentName}.tsx";`
+        );
+        const indexFileRes = await writeFileSync(indexFile.path, indexFileBlob);
+
+        const componentFile = vscode.Uri.joinPath(
+          newDir,
+          `${componentName}.tsx`
+        );
+        const componentFileBlob: Uint8Array = Buffer.from(
+          `import { FC } from 'react';\n\nexport const ${componentName}: FC = () => {\n  return <></>;\n}`
+        );
+        const componentFileRes = await writeFileSync(
+          componentFile.path,
+          componentFileBlob
+        );
+
+        console.log(6, indexFileRes, componentFileRes);
+        // 右下にメッセージを表示
+        vscode.window.showInformationMessage(
+          `Hello World from create-react-component-files! ${componentName}`
+        );
+      } catch (error) {
+        console.error(error);
+        vscode.window.showErrorMessage("コンポーネントの生成に失敗しました。");
+      }
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
